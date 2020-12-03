26837406ca49a864f6d3fa8bf670d6c5f4eb4ea1
okanoamazia <k.okano@amazia.co.jp>
2020-12-03 16:44:09 +0900

    ハッシュの操作

---
+++ ハッシュの操作/2.rb
@@ -0,0 +1,3 @@
+comics = {"A": "100円", "B": "80円", "C": "60円"}
+p comics[:A]
+p comics[:C]
---
+++ ハッシュの操作/5.rb
@@ -0,0 +1,10 @@
+comics = {"A": "100円", "B": "80円", "C": "60円"}
+comics[:A]="500円"
+p comics.values
+
+
+comics = {"A": "100円", "B": "80円", "C": "60円"}
+comics[:D]="500円"
+p comics.values
+
+
